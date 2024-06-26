9daa9d2b321d97501be4265c180f8a6eeeb28de9
je1lee <zerg1173@gmail.com>
2024-06-26 03:15:49 +0000

    wip: llava-phi

--- llava/model/__init__.py
+++ llava/model/__init__.py
@@ -1,10 +1,7 @@
-try:
-    from .language_model.llava_llama import LlavaLlamaForCausalLM, LlavaConfig
-    from .language_model.llava_mpt import LlavaMptForCausalLM, LlavaMptConfig
-    from .language_model.llava_mistral import (
-        LlavaMistralForCausalLM,
-        LlavaMistralConfig,
-    )
-    from .language_model.llava_phi3 import LlavaPhiForCausalLM, LlavaPhiConfig
-except:
-    pass
+from .language_model.llava_llama import LlavaLlamaModel, LlavaConfig
+
+# from .language_model.llava_mpt import LlavaMPTForCausalLM, LlavaMPTConfig
+from .language_model.llava_mistral import LlavaMistralForCausalLM, LlavaMistralConfig
+
+# from .language_model.llava_gemma import LlavaGemmaForCausalLM, LlavaGemmaConfig
+from .language_model.llava_phi3 import LlavaPhiForCausalLM, LlavaPhiConfig
--- llava/train/train.py
+++ llava/train/train.py
@@ -561,9 +561,7 @@ def preprocess_v1(
 
 
 def preprocess_mpt(
-    sources,
-    tokenizer: transformers.PreTrainedTokenizer,
-    has_image: bool = False
+    sources, tokenizer: transformers.PreTrainedTokenizer, has_image: bool = False
 ) -> Dict:
     conv = conversation_lib.default_conversation.copy()
     roles = {"human": conv.roles[0], "gpt": conv.roles[1]}
@@ -585,7 +583,13 @@ def preprocess_mpt(
     # Tokenize conversations
 
     if has_image:
-        input_ids = torch.stack([tokenizer_image_token(prompt, tokenizer, return_tensors='pt') for prompt in conversations], dim=0)
+        input_ids = torch.stack(
+            [
+                tokenizer_image_token(prompt, tokenizer, return_tensors="pt")
+                for prompt in conversations
+            ],
+            dim=0,
+        )
     else:
         input_ids = tokenizer(
             conversations,
@@ -606,7 +610,9 @@ def preprocess_mpt(
         rounds = conversation.split(conv.sep)
         re_rounds = [conv.sep.join(rounds[:3])]  # system + user + gpt
         for conv_idx in range(3, len(rounds), 2):
-            re_rounds.append(conv.sep.join(rounds[conv_idx:conv_idx+2]))    # user + gpt
+            re_rounds.append(
+                conv.sep.join(rounds[conv_idx : conv_idx + 2])
+            )  # user + gpt
         cur_len = 0
         target[:cur_len] = IGNORE_INDEX
         for i, rou in enumerate(re_rounds):
@@ -625,7 +631,11 @@ def preprocess_mpt(
                 round_len = len(tokenizer(rou).input_ids)
                 instruction_len = len(tokenizer(parts[0]).input_ids) - 1
 
-            if i != 0 and getattr(tokenizer, 'legacy', False) and IS_TOKENIZER_GREATER_THAN_0_14:
+            if (
+                i != 0
+                and getattr(tokenizer, "legacy", False)
+                and IS_TOKENIZER_GREATER_THAN_0_14
+            ):
                 round_len += 1
                 instruction_len += 1
 
@@ -649,9 +659,7 @@ def preprocess_mpt(
 
 
 def preprocess_phi3(
-    sources,
-    tokenizer: transformers.PreTrainedTokenizer,
-    has_image: bool = False
+    sources, tokenizer: transformers.PreTrainedTokenizer, has_image: bool = False
 ) -> Dict:
     conv = conversation_lib.default_conversation.copy()
     roles = {"human": conv.roles[0], "gpt": conv.roles[1]}
@@ -673,7 +681,13 @@ def preprocess_phi3(
     # Tokenize conversations
 
     if has_image:
-        input_ids = torch.stack([tokenizer_image_token(prompt, tokenizer, return_tensors='pt') for prompt in conversations], dim=0)
+        input_ids = torch.stack(
+            [
+                tokenizer_image_token(prompt, tokenizer, return_tensors="pt")
+                for prompt in conversations
+            ],
+            dim=0,
+        )
     else:
         input_ids = tokenizer(
             conversations,
@@ -694,7 +708,9 @@ def preprocess_phi3(
         rounds = conversation.split(conv.sep)
         re_rounds = [conv.sep.join(rounds[:3])]  # system + user + gpt
         for conv_idx in range(3, len(rounds), 2):
-            re_rounds.append(conv.sep.join(rounds[conv_idx:conv_idx+2]))    # user + gpt
+            re_rounds.append(
+                conv.sep.join(rounds[conv_idx : conv_idx + 2])
+            )  # user + gpt
         cur_len = 0
         target[:cur_len] = IGNORE_INDEX
         for i, rou in enumerate(re_rounds):
@@ -720,7 +736,11 @@ def preprocess_phi3(
                 round_len -= 2
                 instruction_len -= 2
 
-            if i != 0 and getattr(tokenizer, 'legacy', False) and IS_TOKENIZER_GREATER_THAN_0_14:
+            if (
+                i != 0
+                and getattr(tokenizer, "legacy", False)
+                and IS_TOKENIZER_GREATER_THAN_0_14
+            ):
                 round_len += 1
                 instruction_len += 1
 
